08da88fff7f32cf5481fefcb47a1117e1c12578f
divya-mehra <divyahmehra@gmail.com>
2023-07-23 16:06:49 -0400

    styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Splash from './templates/Splash'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Splash from "./templates/Splash";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
 
+const theme = createTheme({
+  palette: { primary: { main: "#000" }, secondary: { main: "#ccc" } },
+  typography: { variantOne: { fontSize: "6rem", color: red[200] } },
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Splash/>
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Splash />
+    </ThemeProvider>
+  </React.StrictMode>
+);
--- src/templates/Splash.jsx
+++ src/templates/Splash.jsx
@@ -1,8 +1,23 @@
+import { Button, Typography, Box } from "@mui/material";
+import styled from "@emotion/styled";
+
 const Splash = () => {
+  const CustomButton = styled(Button)({ padding: 4 * 2, margin: 4 * 4 });
+
   return (
-    <div>
-      <p>Welcome to The Solution</p>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        p: 4,
+        textAlign: "center",
+        justifyContent: "center"
+      }}
+    >
+      <Typography variant="variantOne">Welcome to The Solution</Typography>
+      <Typography>We're thrilled to have you here.</Typography>
+      <CustomButton variant="contained"> Enter </CustomButton>
+    </Box>
   );
 };
 
