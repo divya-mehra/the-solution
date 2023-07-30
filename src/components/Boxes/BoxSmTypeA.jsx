e89535237ea268bdd4b7a6f4a35319bd0ec9ffd5
divya-mehra <divyahmehra@gmail.com>
2023-07-30 19:50:19 -0400

    boxtypeb layout changes

--- src/components/Boxes/BoxSmTypeA.jsx
+++ src/components/Boxes/BoxSmTypeA.jsx
@@ -3,9 +3,8 @@ import styled from "@emotion/styled";
 
 const BoxSmTypeA = ({ number, title, description }) => {
   const SmallBox = styled(Box)({
-    border: "1px solid black",
-    flex: "1 1 1 0",
-    aspectRatio: "1/1",
+    border: "0.5px solid black",
+    aspectRatio: "3/2",
     flexGrow: "1",
     padding: 4 * 4,
   });
