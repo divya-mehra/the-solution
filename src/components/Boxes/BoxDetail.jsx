74df2a60a0501d287349d69d02172b527dab8c71
divya-mehra <divyahmehra@gmail.com>
2023-07-31 16:43:16 -0400

    intersection observer

--- src/components/Boxes/BoxDetail.jsx
+++ src/components/Boxes/BoxDetail.jsx
@@ -2,7 +2,12 @@ import { Box } from "@mui/material";
 import styles from "./boxes.module.css";
 
 const BoxDetail = () => {
-  return <Box className={styles.boxDetail}></Box>;
+  return (
+  <Box className={styles.boxDetail}>
+  <Box className={styles.boxDetailElement}>What's Inside</Box>
+  <Box className={styles.boxDetailElement}>Best For</Box>
+  </Box>
+  )
 };
 
 export default BoxDetail;
--- src/components/Boxes/BoxLgWithObserver.jsx
+++ src/components/Boxes/BoxLgWithObserver.jsx
@@ -2,7 +2,17 @@ import { Box } from "@mui/material";
 import styles from './boxes.module.css'
 
 const BoxLgWithObserver = () => {
-  return <Box className={`${styles.boxLgWithObserver} ${styles.boxLg}`}></Box>;
+  return <Box className={`${styles.boxLgWithObserver} ${styles.boxLg}`}>
+
+<div style={{
+    position: "sticky",
+    top: 0,
+    height: "100vh"
+    }}>1</div>
+<div>2</div>
+
+
+  </Box>;
 };
 
 export default BoxLgWithObserver;
