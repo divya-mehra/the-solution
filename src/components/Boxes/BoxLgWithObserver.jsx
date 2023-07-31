355887c9dcd92be2d0c5aefc4afd62db26d21051
divya-mehra <divyahmehra@gmail.com>
2023-07-31 17:29:23 -0400

    test intersection observer with extra shapes

--- src/components/Boxes/BoxLgWithObserver.jsx
+++ src/components/Boxes/BoxLgWithObserver.jsx
@@ -1,5 +1,8 @@
 import { Box } from "@mui/material";
-import styles from './boxes.module.css'
+import styles from './boxes.module.css';
+import imageOne from '../../assets/red.png'
+import imageTwo from '../../assets/redblue.png'
+import imageThree from '../../assets/redblueyellow.png'
 
 const BoxLgWithObserver = () => {
   return <Box className={`${styles.boxLgWithObserver} ${styles.boxLg}`}>
@@ -7,9 +10,21 @@ const BoxLgWithObserver = () => {
 <div style={{
     position: "sticky",
     top: 0,
-    height: "100vh"
-    }}>1</div>
-<div>2</div>
+    height: "100vh",
+    }}>
+        <img src={imageOne} alt="Image One" style={{ width: "100%", objectFit: "cover" }}/>
+
+    </div>
+<div style={{
+    display: "flex",
+    flexDirection: "column"
+}}
+>
+    <div className={styles.observerTextPanel}></div>
+    <div className={styles.observerTextPanel}></div>
+    <div className={styles.observerTextPanel}></div>
+    <div className={styles.observerTextPanel}></div>
+</div>
 
 
   </Box>;
