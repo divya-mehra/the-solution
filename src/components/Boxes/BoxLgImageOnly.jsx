67209ce45db580e3f58cef26468ab931bc83004d
divya-mehra <divyahmehra@gmail.com>
2023-08-01 21:38:47 -0400

    glitch on hover

--- src/components/Boxes/BoxLgImageOnly.jsx
+++ src/components/Boxes/BoxLgImageOnly.jsx
@@ -1,6 +1,7 @@
 import styles from "./boxes.module.css";
 import { Box, Typography } from "@mui/material";
-import LookHero from '../../assets/look_hero.jpg'
+import LookHero from "../../assets/look_hero.jpg";
+import { useEffect, useState } from "react";
 
 {
   /* <Typography variant="hero">
@@ -12,40 +13,50 @@ import LookHero from '../../assets/look_hero.jpg'
 
 const BoxLgImageOnly = () => {
 
-    const mouseOverHandler = () => {
-        setTimeout(()=> {}, 2000)
-    }
+  const [showTextCover, setShowTextCover] = useState(false);
+
+  const mouseOverHandler = () => {
+    setTimeout(() => {setShowTextCover(true)}, 2000);
+  };
+
+  const mouseOutHandler = () => {
+    setShowTextCover(false)
+  };
 
   return (
     <div
       className={`${styles.boxLgImageOnly} `}
       style={{
         backgroundImage: `url(${LookHero})`,
-        backgroundColor: 'rgba(0, 0, 0, 0.4)',
-        color: '#ffffff40',
-        opacity: 1
+        backgroundColor: "rgba(0, 0, 0, 0.4)",
+        color: "#ffffff40",
+        opacity: showTextCover ? 0 : 1,
+        transition: "ease all 1s"
       }}
       onMouseOver={mouseOverHandler}
+      onMouseOut={mouseOutHandler}
     >
-        
-        
-        <div
+      <div
         className="textCover"
         style={{
-          backgroundColor: 'rgba(255, 255, 255, 0.95)',
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
+          backgroundColor: "rgba(255, 0, 255, 0.95)",
+          width: "100%",
+          height: "100%",
+          position: "absolute",
           top: 0,
           left: 0,
-          opacity: 0
-        }}>
-<Typography variant="hero" sx={{color: '#000'}}>A woman looks on, her eyes closed. She is serene. She is fierce. She is zen. She is beautiful. <span style={{color: "#ff0000"}}>Her skin sparkles</span> like a Christmas ornament. She is surrounded my saturated fruit and leaves.</Typography>   
-        </div>
-      
+          opacity: showTextCover ? 1 : 0,
+          transition: "ease all 1s"
 
-
-        
+        }}
+      >
+        <Typography variant="hero" sx={{ color: "#000" }}>
+          A woman looks on, her eyes closed. She is serene. She is fierce. She
+          is zen. She is beautiful.{" "}
+          <span style={{ color: "#ff0000" }}>Her skin sparkles</span> like a
+          Christmas ornament. She is surrounded my saturated fruit and leaves.
+        </Typography>
+      </div>
     </div>
   );
 };
