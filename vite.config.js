e63d267de2d21aae3ae662250d1a447f268bdaf1
divya-mehra <divyahmehra@gmail.com>
2023-08-21 22:23:02 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/the-solution.co/',
+  // base: '/',
+  // base: '/the-solution.co/',
+  base: '/dist/',
 })
