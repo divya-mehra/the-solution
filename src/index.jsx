e1bd7f2879b550ece8ccb5d47c47ced5f05ea397
divya-mehra <divyahmehra@gmail.com>
2023-12-29 21:02:38 +0530

    updates

--- src/index.jsx
+++ src/index.jsx
@@ -43,8 +43,8 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+    {/* <BrowserRouter> */}
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         {/* Replaces MemoryRouter with BrowserRouter */}
         <ScrollToTop />
         {/* This will scroll the user to the top on route changes */}
